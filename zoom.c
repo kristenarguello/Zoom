d2b4ad51c69511ff04fbecf891782b174ed63a3d
Kristen <kristenarguello20@gmail.com>
2023-04-20 10:39:55 -0300

    Trabalho feito

--- zoom.c
+++ zoom.c
@@ -9,53 +9,83 @@ int main()
 	struct image_s dataNew;
 	struct image_s *imageNew = &dataNew;
 
-	int i, j, r, r1, h, w;
+	int i, j, r, r1, h, w, red, green, blue;
 
 	r1 = read_ppm("lena.ppm", image);
-	if (r1 == 0)
-	{
+	if (r1 == 0) {
 		h = image->height;
 		w = image->width;
-
-		printf("width: %d, height: %d\n", image->width, image->height);
-		for (j = 0; j < image->height; j++)
-		{
-			
-			for (i = 0; i < image->width; i++)
-			{
-				printf("(%d %d) %d %d %d\n", i, j,
-					   image->pix[j * image->width + i].r,
-					   image->pix[j * image->width + i].g,
-					   image->pix[j * image->width + i].b);
-
-			}//printa os pixels
-		}
-
-		// image->pix[50 * image->width + 20].r = 255;
-		// image->pix[50 * image->width + 20].g = 255;
-		// image->pix[50 * image->width + 20].b = 255;
-
-		write_ppm("lena_copy.ppm", image);//faz o arquivo no copy
-
+		r = new_ppm(imageNew, w*3, h*3);
 	}
 
-	if (r1 == 0) 
-		r = new_ppm(imageNew, w*3, h*3);//cria unma nova imagem do 0
+	if (r == 0 && r1 == 0) {
+		for (j = 0; j < image->height; j++) {
+			for (i = 0; i < image->width; i++) {
+				
+				red = image->pix[j * image->width + i].r;
+				green = image->pix[j * image->width + i].g;
+				blue = image->pix[j * image->width + i].b;
+				
+				// red
+				for (int k = 0; k < 3; k++) {
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3].r = 0;
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3].g = 0;
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3].b = 0;
+				}
+				if (red > 74 && red < 135) {
+					imageNew->pix[(j * 3 + 1) * imageNew->width + i * 3].r = 255;
+				}
+				else if (red > 134 && red < 180) {
+					imageNew->pix[(j * 3) * imageNew->width + i * 3].r = 255;
+					imageNew->pix[(j * 3 + 2) * imageNew->width + i * 3].r = 255;
+				}
+				else if (red > 179 && red < 256){
+					imageNew->pix[(j * 3) * imageNew->width + i * 3].r = 255;
+					imageNew->pix[(j * 3 + 1) * imageNew->width + i * 3].r = 255;
+					imageNew->pix[(j * 3 + 2) * imageNew->width + i * 3].r = 255;
+				}
 
-	if (r == 0 && r1 == 0)
-	{
-		for (j = 0; j < image->height; j++)
-		{
-			for (i = 0; i < image->width; i++)
-			{
-				imageNew->pix[j * imageNew->width + i].r = image->pix[j * image->width + i].r;
-				imageNew->pix[j * imageNew->width + i].g = image->pix[j * image->width + i].g;
-				imageNew->pix[j * imageNew->width + i].b = image->pix[j * image->width + i].b;
+				// green
+				for (int k = 0; k < 3; k++) {
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3 + 1].r = 0;
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3 + 1].g = 0;
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3 + 1].b = 0;
+				}
+				if (green > 74 && green < 135) {
+					imageNew->pix[(j * 3 + 1) * imageNew->width + i * 3 + 1].g = 255;
+				}
+				else if (green > 134 && green < 180) {
+					imageNew->pix[(j * 3) * imageNew->width + i * 3 + 1].g = 255;
+					imageNew->pix[(j * 3 + 2) * imageNew->width + i * 3 + 1].g = 255;
+				}
+				else if (green > 179 && green < 256){
+					imageNew->pix[(j * 3) * imageNew->width + i * 3 + 1].g = 255;
+					imageNew->pix[(j * 3 + 1) * imageNew->width + i * 3 + 1].g = 255;
+					imageNew->pix[(j * 3 + 2) * imageNew->width + i * 3 + 1].g = 255;
+				}
+
+				// blue
+				for (int k = 0; k < 3; k++) {
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3 + 2].r = 0;
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3 + 2].g = 0;
+					imageNew->pix[(j * 3 + k) * imageNew->width + i * 3 + 2].b = 0;
+				}
+				if (blue > 74 && blue < 135) {
+					imageNew->pix[(j * 3 + 1) * imageNew->width + i * 3 + 2].b = 255;
+				}
+				else if (blue > 134 && blue < 180) {
+					imageNew->pix[(j * 3) * imageNew->width + i * 3 + 2].b = 255;
+					imageNew->pix[(j * 3 + 2) * imageNew->width + i * 3 + 2].b = 255;
+				}
+				else if (blue > 179 && blue < 256) {
+					imageNew->pix[(j * 3) * imageNew->width + i * 3 + 2].b = 255;
+					imageNew->pix[(j * 3 + 1) * imageNew->width + i * 3 + 2].b = 255;
+					imageNew->pix[(j * 3 + 2) * imageNew->width + i * 3 + 2].b = 255;
+				}
 			}
 		}
-
-
-		write_ppm("test.ppm", imageNew);
+		write_ppm("zoomNovo.ppm", imageNew);
+		printf("Imagem com zoom 3x criada com sucesso!");
 
 		free_ppm(image);
 		free_ppm(imageNew);
